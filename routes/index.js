f97b2ddb49be74ab6ae29e87c74b2a34988e5707
BathlaNeffi <neffybathla@gmail.com>
2023-11-30 16:48:01 +0530

    Thrid  Commit -This commit is to revert this

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const homeController=require('../controller/home_controller');
 // routing the request to home controller
 router.get('/',homeController.home);
 
+console.log('Added this on line 8 and route>index.js')
+
 
 
 module.exports=router;
