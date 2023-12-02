c3985f5a55e834489b0f031446db35ba463e23a3
BathlaNeffi <neffybathla@gmail.com>
2023-12-02 14:24:37 +0530

    setup signin page and sign up page

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -4,3 +4,26 @@ module.exports.users=(req,res)=>{
         title:"Neffi's Profile"
     })
 }
+// render the Sign-In page
+
+module.exports.signIn=(req,res)=>{
+    res.render('user_sign_in',{
+        title:'Codeial | Sign In'
+    });
+}
+// render the Sign-Up page
+module.exports.signUp=(req,res)=>{
+    res.render('user_sign_up',{
+        title:'Codeial | Sign Up'
+    });
+}
+
+// Action for user create
+module.exports.create=(req,res)=>{
+    // To be done latter
+}
+
+// action for create Session
+module.exports.createSession=(req,res)=>{
+    // To de done latter
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express =require('express');
-
 const router =express.Router();
-
 const usersController = require('../controller/users_controller');
 
 router.get('/profile',usersController.users);
+router.get('/sign-in',usersController.signIn);
+router.get('/sign-up',usersController.signUp);
 
 module.exports=router;
