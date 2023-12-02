3f630ab58065987048a415ead1e25b908eb1bcd7
BathlaNeffi <neffybathla@gmail.com>
2023-12-02 11:32:24 +0530

    setup static files for layout and other sub pages

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,6 +1,6 @@
 module.exports.users=(req,res)=>{
     // res.end('<h1>Users Profile</h1>')
-    return res.render('users',{
+    return res.render('user_profile',{
         title:"Neffi's Profile"
     })
 }
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app=express();
 const port = 8000;
-const routes= require('./routes');
 const path= require('path');
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
 
 // use the express router
+const routes= require('./routes');
 app.use('/',routes);
 
 // setting up the view engine
@@ -13,6 +17,10 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'./views'));
 // console.log(path.join(__dirname,'./views'));
 
+app.use(express.static('./assets'));
+// extract style and scripts from sub pages into the payout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.listen(port,function(err){
     if(err){
